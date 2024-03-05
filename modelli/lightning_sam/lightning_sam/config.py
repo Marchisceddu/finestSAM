7fa2230e1251cb4021b33f6730c89bfec0f125a8
Marchisceddu <marcopilia02@gmail.com>
2024-03-05 14:36:01 +0100

    cambio conf

--- modelli/lightning_sam/lightning_sam/config.py
+++ modelli/lightning_sam/lightning_sam/config.py
@@ -2,14 +2,14 @@ from box import Box
 
 config = {
     "num_devices": "auto",
-    "batch_size": 12,
+    "batch_size": 1,
     "num_workers": 2,
     "num_epochs": 100,
     "k_fold": 5,
     "eval_interval": 10,
     "out_dir": "out/training",
     "opt": {
-        "learning_rate": 4e-4,
+        "learning_rate": 8e-4,
         "weight_decay": 1e-4,
         "decay_factor": 10,
         "steps": [60000, 86666],
@@ -20,7 +20,7 @@ config = {
         "checkpoint": "../sav/sam_vit_h_4b8939.pth",
         "freeze": {
             "image_encoder": True,
-            "prompt_encoder": True,
+            "prompt_encoder": False,
             "mask_decoder": False,
         },
     },
