f0b558dba27fba262c833485c07ff04b26ef9f80
Druimo <dessis2002@gmail.com>
2024-04-03 16:53:54 +0200

    Nuove loss, punti random

--- shape_SAM/model/losses.py
+++ shape_SAM/model/losses.py
@@ -2,8 +2,40 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-ALPHA = 0.8
+ALPHA = 0.7
 GAMMA = 2
+
+
+class DiceLoss(nn.Module):
+    
+    def __init__(self, weight=None, size_average=True):
+        super().__init__()
+
+    def forward(self, inputs, targets,num_masks, smooth=1):
+        """
+        Compute the DICE loss, similar to generalized IOU for masks
+        Args:
+            inputs: A float tensor of arbitrary shape.
+                    The predictions for each example.
+            targets: A float tensor with the same shape as inputs. Stores the binary
+                    classification label for each element in inputs
+                    (0 for the negative class and 1 for the positive class).
+            num_masks: Number of masks in the batch
+        """
+
+        inputs = inputs.squeeze()
+        targets = targets.squeeze()
+
+        inputs = inputs.sigmoid()
+        inputs = inputs.flatten(1)
+        targets = targets.flatten(1)
+
+        numerator = 2 * (inputs * targets).sum(1)
+        denominator = inputs.sum(-1) + targets.sum(-1)
+        loss = 1 - (numerator + smooth) / (denominator + smooth)
+        
+        return loss.sum() / num_masks
+
 
 
 class FocalLoss(nn.Module):
@@ -11,38 +43,42 @@ class FocalLoss(nn.Module):
     def __init__(self, weight=None, size_average=True):
         super().__init__()
 
-    def forward(self, inputs, targets, alpha=ALPHA, gamma=GAMMA, smooth=1):
-        inputs = F.sigmoid(inputs)
-        inputs = torch.clamp(inputs, min=0, max=1)
-        #flatten label and prediction tensors
-        inputs = inputs.view(-1)
-        targets = targets.view(-1)
+    def forward(self, inputs, targets, num_masks, alpha=ALPHA, gamma=GAMMA):
+        
+        """
+        Loss used in RetinaNet for dense detection: https://arxiv.org/abs/1708.02002.
+        Args:
+            inputs: A float tensor of arbitrary shape.
+                    The predictions for each example.
+            targets: A float tensor with the same shape as inputs. Stores the binary
+                    classification label for each element in inputs
+                    (0 for the negative class and 1 for the positive class).
+            num_masks: Number of masks in the batch
+            
+            alpha: (optional) Weighting factor in range (0,1) to balance
+                    positive vs negative examples. Default = -1 (no weighting).
+            gamma: Exponent of the modulating factor (1 - p_t) to
+                balance easy vs hard examples.
+            Returns:
+                Loss tensor
+        """
+        inputs = inputs.squeeze()
+        targets = targets.squeeze()
 
-        BCE = F.binary_cross_entropy(inputs, targets, reduction='none')
-        BCE_EXP = torch.exp(-BCE)
-        focal_loss = alpha * (1 - BCE_EXP)**gamma * BCE
-        focal_loss = focal_loss.mean()
+        prob = inputs.sigmoid()
+        inputs = inputs.flatten(1)
+        prob = prob.flatten(1)
+        targets = targets.flatten(1)
 
-        return focal_loss
+        ce_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduction="none")
+        p_t = prob * targets + (1 - prob) * (1 - targets)
+        loss = ce_loss * ((1 - p_t) ** gamma)
 
+        if alpha >= 0:
+            alpha_t = alpha * targets + (1 - alpha) * (1 - targets)
+            loss = alpha_t * loss
 
-class DiceLoss(nn.Module):
-
-    def __init__(self, weight=None, size_average=True):
-        super().__init__()
-
-    def forward(self, inputs, targets, smooth=1):
-        inputs = F.sigmoid(inputs)
-        inputs = torch.clamp(inputs, min=0, max=1)
-        #flatten label and prediction tensors
-        inputs = inputs.view(-1)
-        targets = targets.view(-1)
-
-        intersection = (inputs * targets).sum()
-        dice = (2. * intersection + smooth) / (inputs.sum() + targets.sum() + smooth)
-
-        return 1 - dice
-    
+        return loss.mean(1).sum() / num_masks
     
 
 class Calc_iou(nn.Module):
@@ -51,8 +87,7 @@ class Calc_iou(nn.Module):
         super().__init__()
 
     def forward(self, pred_mask: torch.Tensor, gt_mask: torch.Tensor):
-      pred_mask = torch.round(pred_mask - 0.5)
-      pred_mask = torch.clamp(pred_mask, min=0.0, max=1.0).float()
+      pred_mask = (pred_mask >= 0.5).float()
       intersection = torch.sum(torch.mul(pred_mask, gt_mask), dim=(2, 3))
       union = torch.sum(pred_mask, dim=(2,3)) + torch.sum(gt_mask, dim=(2, 3)) - intersection
       epsilon = 1e-7
