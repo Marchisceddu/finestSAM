346b61079f6be45b200fe8e28b8a3a79179a029b
Marco Pilia <78845523+Marchisceddu@users.noreply.github.com>
2024-04-10 16:58:02 +0200

    Update losses.py

--- shape_SAM/model/losses.py
+++ shape_SAM/model/losses.py
@@ -94,63 +94,6 @@ class Calc_iou(nn.Module):
         pred_mask = pred_mask.squeeze()
         gt_mask = gt_mask.squeeze()
         
-        point_coords = []
-        point_labels = []
-
-        
-        for mask, gtm in zip(pred_mask, gt_mask):
-
-            positive_points = cfg.dataset.positive_points
-            negative_points = cfg.dataset.negative_points
-            
-            # Estrazione dei nuovi punti di foreground e background
-            new_foreground_points = (mask == 1.) & (gtm == 1.)
-            new_background_points = (mask == 0.) & (gtm == 0.)
-            
-            # Individua le coordinate dei nuovi punti di foreground e background
-            foreground_coordinates = torch.nonzero(new_foreground_points, as_tuple=True)
-            background_coordinates = torch.nonzero(new_background_points, as_tuple=True)
-            
-            if (len(foreground_coordinates[0]) < positive_points):
-                positive_points = len(foreground_coordinates[0])
-                negative_points = cfg.dataset.negative_points + (cfg.dataset.positive_points - positive_points)
-                
-                
-            if (len(background_coordinates[0]) < negative_points):
-                negative_points = len(background_coordinates[0])
-                positive_points = cfg.dataset.positive_points + (cfg.dataset.negative_points - negative_points)
-                
-
-
-            if not (positive_points == 0):            
-                temp_list_point = []
-                for i in range(0, positive_points):
-                    idx = np.random.randint(0, len(foreground_coordinates[0]))
-                    temp_list_point.append([foreground_coordinates[1][idx].item(), foreground_coordinates[0][idx].item()])
-                new_foreground_points = temp_list_point.copy()
-                list_label_1 = [1] * len(new_foreground_points)
-                
-            else:
-                new_foreground_points = []
-                list_label_1 = []
-            
-            if not (negative_points == 0):
-                temp_list_point = []
-                for i in range(0, negative_points):
-                    idx = np.random.randint(0, len(background_coordinates[0]))
-                    temp_list_point.append([background_coordinates[1][idx].item(), background_coordinates[0][idx].item()])
-                new_background_points = temp_list_point.copy()
-                list_label_0 = [0] * len(new_background_points)
-            else:
-                new_background_points = []
-                list_label_0 = []
-            
-            actual_point_coords = new_foreground_points + new_background_points
-            actual_point_labels = list_label_1 + list_label_0
-            
-            point_coords.append(actual_point_coords)
-            point_labels.append(actual_point_labels)
-        
         pred_mask = pred_mask.flatten(1)
         gt_mask = gt_mask.flatten(1)
             
@@ -159,4 +102,4 @@ class Calc_iou(nn.Module):
         epsilon = 1e-7
         batch_iou = intersection / (union + epsilon)
 
-        return batch_iou, point_coords, point_labels
+        return batch_iou
